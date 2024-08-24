c46a9e1a789611ab0deb6a48db1d92b985437b0f
inokawa <inokawa@users.noreply.github.com>
2024-08-24 11:16:55 +0000

    deploy: 9a013944c30e6f4db96f788fe9268acbb53f0a4c

--- core/src/lib.rs
+++
@@ -1,3 +0,0 @@
-mod synth;
-
-pub use synth::*;
--- web/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use wasm_bindgen::prelude::*;
-
-use core;
-
-#[wasm_bindgen]
-pub fn synth(freq: f32, sec: u32, sample_rate: u32) -> Vec<f32> {
-    core::synth(freq, sec, sample_rate)
-}
