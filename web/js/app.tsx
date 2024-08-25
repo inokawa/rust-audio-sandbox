904e970d84e5b6578b1c888f966c78a5ed12cbfb
inokawa <48897392+inokawa@users.noreply.github.com>
2024-08-25 20:02:32 +0900

    Visualize input

--- web/js/app.tsx
+++ web/js/app.tsx
@@ -1,7 +1,20 @@
-import { useEffect, useState } from "react";
-import { getAudioDevices, startSynth } from "./audio";
+import { useEffect, useRef, useState } from "react";
+import {
+  createAudioContext,
+  getAudioDevices,
+  createAudioStream,
+  runAnalyser,
+} from "./audio";
 
-const DeviceSelector = ({ devices }: { devices: MediaDeviceInfo[] }) => {
+const DeviceSelector = ({
+  devices,
+  onSelectInput,
+  onSelectOutupt,
+}: {
+  devices: MediaDeviceInfo[];
+  onSelectInput: (id: string) => void;
+  onSelectOutupt: (id: string) => void;
+}) => {
   const inputs = devices.filter((d) => d.kind === "audioinput");
   const outputs = devices.filter((d) => d.kind === "audiooutput");
   return (
@@ -13,7 +26,14 @@ const DeviceSelector = ({ devices }: { devices: MediaDeviceInfo[] }) => {
             return (
               <div key={d.deviceId}>
                 <label>
-                  <input type="radio" name="input" />
+                  <input
+                    type="radio"
+                    name="input"
+                    value={d.deviceId}
+                    onChange={() => {
+                      onSelectInput(d.deviceId);
+                    }}
+                  />
                   {d.label}
                 </label>
               </div>
@@ -28,7 +48,14 @@ const DeviceSelector = ({ devices }: { devices: MediaDeviceInfo[] }) => {
             return (
               <div key={d.deviceId}>
                 <label>
-                  <input type="radio" name="output" />
+                  <input
+                    type="radio"
+                    name="output"
+                    value={d.deviceId}
+                    onChange={() => {
+                      onSelectOutupt(d.deviceId);
+                    }}
+                  />
                   {d.label}
                 </label>
               </div>
@@ -41,7 +68,14 @@ const DeviceSelector = ({ devices }: { devices: MediaDeviceInfo[] }) => {
 };
 
 export const App = () => {
+  const CANVAS_WIDTH = 400;
+  const CANVAS_HEIGHT = 400;
+
+  const [input, setInput] = useState<string | null>(null);
+  const [output, setOutput] = useState<string | null>(null);
   const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     (async () => {
@@ -50,10 +84,36 @@ export const App = () => {
   }, []);
 
   return (
-    <div>
-      <DeviceSelector devices={devices} />
+    <div style={{ display: "flex" }}>
       <div>
-        <button onClick={startSynth}>start</button>
+        <DeviceSelector
+          devices={devices}
+          onSelectInput={setInput}
+          onSelectOutupt={setOutput}
+        />
+        <div>
+          <button
+            disabled={!input}
+            onClick={async () => {
+              if (!input) return;
+              const canvasElement = canvasRef.current;
+              if (!canvasElement) return;
+              const canvasContext = canvasElement.getContext("2d")!;
+              const context = createAudioContext();
+              const stream = await createAudioStream(context, input);
+              const cleanup = runAnalyser(context, stream, {
+                width: CANVAS_WIDTH,
+                height: CANVAS_HEIGHT,
+                context: canvasContext,
+              });
+            }}
+          >
+            start
+          </button>
+        </div>
+      </div>
+      <div>
+        <canvas ref={canvasRef} width={CANVAS_WIDTH} height={CANVAS_HEIGHT} />
       </div>
     </div>
   );
--- web/js/audio.ts
+++ web/js/audio.ts
@@ -1,28 +1,115 @@
-window.AudioContext ||= (window as any).webkitAudioContext;
-
 export const getAudioDevices = async () => {
   await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
   return navigator.mediaDevices.enumerateDevices();
 };
 
-export const startSynth = async () => {
-  const wasm = await import("../pkg/index.js");
-
-  const ctx = new AudioContext();
-  const sampleRate = ctx.sampleRate;
-
-  const sec = 1;
-  const resBuf = wasm.synth(440, sec, sampleRate);
-
-  const audioBuffer = ctx.createBuffer(2, sec * sampleRate, sampleRate);
-  audioBuffer.getChannelData(0).set(resBuf);
-  audioBuffer.getChannelData(1).set(resBuf);
-  const source = ctx.createBufferSource();
-  source.buffer = audioBuffer;
-  source.connect(ctx.destination);
-  source.start();
-
-  setTimeout(() => {
-    source.stop();
-  }, 5000);
+export const createAudioContext = (): AudioContext => {
+  return new (AudioContext || (window as any).webkitAudioContext)({
+    latencyHint: "balanced",
+    sampleRate: 44100,
+  });
 };
+
+export const createAudioStream = async (
+  context: AudioContext,
+  deviceId: string
+): Promise<MediaStreamAudioSourceNode> => {
+  const mediaStream = await navigator.mediaDevices.getUserMedia({
+    audio: { deviceId: deviceId },
+    video: false,
+  });
+
+  const source = context.createMediaStreamSource(mediaStream);
+  return source;
+};
+
+const drawPath = (
+  context: CanvasRenderingContext2D,
+  width: number,
+  height: number,
+  dataArray: Uint8Array,
+  bufferLength: number
+) => {
+  context.clearRect(0, 0, width, height);
+
+  context.beginPath();
+
+  const sliceWidth = (width * 1.0) / bufferLength;
+  let x = 0;
+  for (var i = 0; i < bufferLength; i++) {
+    const v = dataArray[i] / 128.0;
+    const y = (v * height) / 2;
+
+    if (i === 0) {
+      context.moveTo(x, y);
+    } else {
+      context.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  }
+
+  context.lineTo(width, height / 2);
+  context.stroke();
+};
+
+export const runAnalyser = (
+  context: AudioContext,
+  source: AudioNode,
+  {
+    context: canvasContext,
+    width,
+    height,
+  }: {
+    context: CanvasRenderingContext2D;
+    width: number;
+    height: number;
+  }
+) => {
+  const analyser = context.createAnalyser();
+  source.connect(analyser);
+
+  analyser.fftSize = 2048;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  let id: number | null = null;
+  const draw = () => {
+    analyser.getByteTimeDomainData(dataArray);
+
+    drawPath(canvasContext, width, height, dataArray, bufferLength);
+
+    id = requestAnimationFrame(draw);
+  };
+
+  draw();
+
+  return () => {
+    analyser.disconnect();
+    if (id != null) {
+      cancelAnimationFrame(id);
+    }
+  };
+};
+
+// export const startSynth = async () => {
+//   const wasm = await import("../pkg/index.js");
+
+//   const ctx = new AudioContext();
+//   const sampleRate = ctx.sampleRate;
+
+//   const sec = 1;
+//   const resBuf = wasm.synth(440, sec, sampleRate);
+
+//   const audioBuffer = ctx.createBuffer(2, sec * sampleRate, sampleRate);
+//   audioBuffer.getChannelData(0).set(resBuf);
+//   audioBuffer.getChannelData(1).set(resBuf);
+//   const source = ctx.createBufferSource();
+//   source.buffer = audioBuffer;
+//   source.connect(ctx.destination);
+//   source.start();
+
+//   setTimeout(() => {
+//     source.stop();
+//   }, 5000);
+// };
